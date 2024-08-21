5a964e36860ffc388a26416cba759fe260e1219f
nhannht <nhanclassroom@gmail.com>
2024-08-22 00:16:17 +0700

    feat: fuck em, just add cloudflare cdn for polyfills

--- client/main.tsx
+++ client/main.tsx
@@ -6,19 +6,8 @@ import router from "./router";
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "../dev";
 import 'react-quill/dist/quill.snow.css';
-import ResizeObserver from 'resize-observer-polyfill';
-//@ts-ignore
-const ro = new ResizeObserver((entries, observer) => {
-    for (const entry of entries) {
-        const {left, top, width, height} = entry.contentRect;
 
-        console.log('Element:', entry.target);
-        console.log(`Element's size: ${ width }px x ${ height }px`);
-        console.log(`Element's paddings: ${ top }px ; ${ left }px`);
-    }
-});
 
-ro.observe(document.body);
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <DevSupport ComponentPreviews={ComponentPreviews}
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 import * as path from "node:path";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 import legacy from '@vitejs/plugin-legacy'
-import ResizeObserver from "resize-observer-polyfill"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(),
     // legacy({
     //     targets: ['safari >= 12, chrome >= 64'],
-    //     additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    //     polyfills: ['ResizeObserver'],
     //     modernPolyfills:true
     // })
     ],
